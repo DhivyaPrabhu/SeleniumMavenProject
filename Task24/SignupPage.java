3801bf96e33faed6819d92fc44b25992963ff0d5
DhivyaPrabhu <167868211+DhivyaPrabhu@users.noreply.github.com>
2024-06-21 18:38:16 +0530

    Add files via upload

---
+++ Task24/SignupPage.java
@@ -0,0 +1,73 @@
+package DemoblazePages;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.time.Duration;
+
+public class SignupPage
+{
+    WebDriver driver;
+    String Alertmessage;
+    Alert myalert;
+
+    //Constructor to initialize WebDriver
+    public SignupPage(WebDriver driver)
+    {
+        this.driver=driver;
+        PageFactory.initElements(driver,this);
+    }
+
+    //Web elements using @FindBy annotation
+    @FindBy(id="sign-username")
+    WebElement UserNameInput;
+    @FindBy(id="sign-password")
+    WebElement PasswordInput;
+    @FindBy(xpath = "//button[normalize-space()='Sign up']")
+    WebElement SignUpButton;
+    @FindBy(xpath="//div[@id='signInModal']//button[text()='Close']")
+    WebElement CloseButton;
+
+    // Methods to interact with web elements
+    public void EnterUserName(String username)
+    {
+        UserNameInput.sendKeys(username);
+    }
+    public void EnterPassword(String password)
+    {
+        PasswordInput.sendKeys(password);
+    }
+    public void ClickSignUpButton()
+    {
+        SignUpButton.click();
+        WebDriverWait mywait=new WebDriverWait(driver,Duration.ofSeconds(10));
+
+        // Handle alert
+            mywait.until(ExpectedConditions.alertIsPresent());
+            myalert=driver.switchTo().alert();
+            Alertmessage=myalert.getText();
+            myalert.accept();// Accept the alert (close it)
+
+    }
+    public void verifySignUp(String expectedMessage)
+    {
+       // Assert.assertEquals(Alertmessage,expectedMessage);
+        if (Alertmessage.equals(expectedMessage))
+        {
+            System.out.println("Sign up successful: " + Alertmessage);
+        }
+        else
+        {
+            System.out.println("Sign up failed: Expected message '" + expectedMessage + "', but got '" + Alertmessage + "'");
+            throw new AssertionError("Sign up message does not match expected message.");
+        }
+
+    }
+
+}
